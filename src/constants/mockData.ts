abfe01e7e79d60f631ffb1e608236efa4ddc0ba1
sabndev <sabenih726@gmail.com>
2026-06-12 16:46:00 +0700

    Update mockData.ts

--- src/constants/mockData.ts
+++ src/constants/mockData.ts
@@ -1,7 +1,24 @@
-import type {
-  Transaction,
-  CategorySpending,
-  MonthlyTrend,
-} from '../types'
+import type { Transaction } from '../types'
 
+export const STORAGE_KEY = 'wealthvibe_transactions'
+
+// Data dummy sudah dihapus, aplikasi mulai dari nol
 export const DEFAULT_TRANSACTIONS: Transaction[] = []
+
+export const DEFAULT_BUDGET = 5000000
+
+export const QUICK_AMOUNTS = [
+  5000,
+  10000,
+  25000,
+  50000,
+]
+
+export const PAYMENT_OPTIONS = [
+  'Cash',
+  'Debit BCA',
+  'GoPay',
+  'OVO',
+  'ShopeePay',
+  'DANA',
+]
